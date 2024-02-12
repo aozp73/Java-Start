e59da83b88c7c5389c394fcdd1ffa9a405053465
aozp73 <aozp73@naver.com>
2024-02-12 15:39:45 +0900

    실행 / 주석

---
+++ src/CommentJava.java
@@ -0,0 +1,12 @@
+public class CommentJava {
+
+    public static void main(String[] args) {
+        System.out.println("hello java1"); // 주석 1
+        //System.out.println("hello java2");
+
+        /* 주석 2
+        System.out.println("hello java3");
+        System.out.println("hello java4");
+         */
+    }
+}
